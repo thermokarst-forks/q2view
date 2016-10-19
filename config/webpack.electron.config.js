f155f1551b521520e798a6827092e563c2a43f45
Matthew Dillon <matthewrdillon@gmail.com>
2016-10-19 16:34:57 -0700

    SQUASH: Some initial conf (doesn't actually work)

---
+++ config/webpack.electron.config.js
@@ -0,0 +1,35 @@
+// ----------------------------------------------------------------------------
+// Copyright (c) 2016--, QIIME development team.
+//
+// Distributed under the terms of the Modified BSD License.
+//
+// The full license is in the file LICENSE, distributed with this software.
+// ----------------------------------------------------------------------------
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extendConfig = require('./webpack.shared');
+
+
+module.exports = extendConfig((config) => {
+    return {
+        output: {
+            path: path.resolve(__dirname, '../electron-build/view'),
+            filename: 'js/bundle.js'
+        },
+        plugins: [...config.plugins,
+            new HtmlWebpackPlugin({
+                template: 'app/index.html',
+                inject: true
+            }),
+            new ExtractTextPlugin('css/main.css'),
+            new CopyWebpackPlugin([
+                { from: 'electron/main.js', to: '../main.js' },
+                { from: 'package.json', to: '../package.json' },
+            ]),
+        ]
+    };
+}, false);
